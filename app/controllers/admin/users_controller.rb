230988626797de1399620d010c0829d82f1ec1db
Cindy Liu <cindyliu923@gmail.com>
2018-04-22 22:39:54 +0800

    setup for admin user index and update

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -2,4 +2,19 @@ class Admin::UsersController < ApplicationController
   before_action :authenticate_user!
   before_action :authenticate_admin
 
+  def index
+    @users = User.all
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(role: params[:role])
+      redirect_to admin_users_path
+      flash[:notice] = "role was successfully updated"
+    else
+      @users = User.all
+      render :index
+    end
+  end
+
 end
