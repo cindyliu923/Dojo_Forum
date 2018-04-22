7e8b496fe7dc978eda28158067b43e5dc92bc3d2
Cindy Liu <cindyliu923@gmail.com>
2018-04-22 15:15:57 +0800

    create viewed model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180422070745) do
+ActiveRecord::Schema.define(version: 20180422071350) do
 
   create_table "posts", force: :cascade do |t|
     t.string "title"
@@ -55,6 +55,14 @@ ActiveRecord::Schema.define(version: 20180422070745) do
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  end
+
+  create_table "vieweds", force: :cascade do |t|
+    t.integer "post_id"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["post_id", "user_id"], name: "index_vieweds_on_post_id_and_user_id"
   end
 
 end
