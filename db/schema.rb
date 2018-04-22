76060ef91320331b27bbcc592f7a47107c5f7c21
Cindy Liu <cindyliu923@gmail.com>
2018-04-22 15:34:36 +0800

    create friendship model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180422071926) do
+ActiveRecord::Schema.define(version: 20180422072209) do
 
   create_table "categories", force: :cascade do |t|
     t.string "name"
@@ -24,6 +24,15 @@ ActiveRecord::Schema.define(version: 20180422071926) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["post_id", "user_id"], name: "index_collects_on_post_id_and_user_id"
+  end
+
+  create_table "friendships", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "friend_id"
+    t.string "status", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id", "friend_id"], name: "index_friendships_on_user_id_and_friend_id"
   end
 
   create_table "posts", force: :cascade do |t|
