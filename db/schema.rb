96a4a0ce28b614a8b15e5ab49201381357786e60
Cindy Liu <cindyliu923@gmail.com>
2018-04-22 15:10:47 +0800

    create reply model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180422064419) do
+ActiveRecord::Schema.define(version: 20180422070745) do
 
   create_table "posts", force: :cascade do |t|
     t.string "title"
@@ -25,6 +25,15 @@ ActiveRecord::Schema.define(version: 20180422064419) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_posts_on_user_id"
+  end
+
+  create_table "replies", force: :cascade do |t|
+    t.text "comment"
+    t.integer "post_id"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["post_id", "user_id"], name: "index_replies_on_post_id_and_user_id"
   end
 
   create_table "users", force: :cascade do |t|
