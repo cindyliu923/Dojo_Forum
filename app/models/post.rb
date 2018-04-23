251f86d99d8d7097e912cc210e3b0c3955d4380c
Cindy Liu <cindyliu923@gmail.com>
2018-04-23 23:10:39 +0800

    setup relation between user and friendship/post, user and setup index for no login user

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,13 @@
 class Post < ApplicationRecord
   belongs_to :category
+  belongs_to :user
 
   has_many :replies, dependent: :destroy
 
   has_many :vieweds, dependent: :destroy
+
+  def self.all_publish 
+    where( :status => 'publish', :permit => 'all' ).all
+  end
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,11 +4,16 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
+  has_many :posts, dependent: :destroy
+
   has_many :replies, dependent: :restrict_with_error
   has_many :replied_posts, through: :replies, source: :post
 
   has_many :vieweds, dependent: :restrict_with_error
   has_many :viewed_posts, through: :vieweds, source: :post
+
+  has_many :friendships, dependent: :destroy
+  has_many :friends, through: :friendships
 
   def admin?
     self.role == "admin"
