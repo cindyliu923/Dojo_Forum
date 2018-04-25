fa239882eb11eabfdcc40b77132100b08498427f
Cindy Liu <cindyliu923@gmail.com>
2018-04-26 00:22:19 +0800

    user on index page can select posts by category

--- app/models/category.rb
+++ app/models/category.rb
@@ -2,4 +2,6 @@ class Category < ApplicationRecord
   validates_presence_of :name
 
   has_many :category_of_posts, dependent: :restrict_with_error
+  has_many :posts, through: :category_of_posts
+
 end
