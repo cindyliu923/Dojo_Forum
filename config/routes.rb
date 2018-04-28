7fc98e418220a6d52860680cf9f1a3b2885ed7bb
Cindy Liu <cindyliu923@gmail.com>
2018-04-28 09:04:41 +0800

    setup routes for my drafts, posts, comments, collects and friends

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,11 @@ Rails.application.routes.draw do
   devise_for :users
   resources :users, only: [:show, :edit, :update] do
     member do
-      get :draft
+      get :drafts
+      get :posts
+      get :comments
+      get :collects
+      get :friends
     end
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
