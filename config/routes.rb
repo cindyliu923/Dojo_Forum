afd49b98f2d82574f80a6e4ce9cb7d3273091234
Cindy Liu <cindyliu923@gmail.com>
2018-04-30 01:17:15 +0800

    setup user add friend button / modify routes for ignore, connect friend

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+# For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   devise_for :users
   resources :users, only: [:show, :edit, :update] do
     member do
@@ -9,8 +10,12 @@ Rails.application.routes.draw do
       get :friends
     end
   end
-  resources :friendships, only: [:create, :destroy, :update]
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :friendships, only: [:create, :destroy] do
+    member do
+      patch :ignore
+      patch :connect
+    end
+  end
   resources :posts do 
     resources :replies, only: [:create, :destroy, :edit, :update]
     collection do
