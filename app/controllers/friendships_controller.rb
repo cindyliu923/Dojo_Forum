e721c225b3e1735308d7d0b72e9a166939950bae
Cindy Liu <cindyliu923@gmail.com>
2018-05-03 22:34:47 +0800

    setup accept, ignore friend request for AJAX

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -1,4 +1,7 @@
 class FriendshipsController < ApplicationController
+  before_action :authenticate_user!
+  before_action :find_friend, only: [:ignore, :connect]
+
   def create
     @friendship = current_user.friendships.build(friend_id: params[:friend_id], status: 'send')
     if @friendship.save
@@ -12,23 +15,23 @@ class FriendshipsController < ApplicationController
 
   def ignore
     Friendship.where(user_id: params[:friend_id], friend_id: current_user).update(status: 'ignore')
-    redirect_back(fallback_location: root_path)
   end
 
   def connect
-    if Friendship.where(user_id: params[:friend_id], friend_id: current_user).update(status: 'connect')
-      flash[:notice] = "Successfully be friend!"
-      redirect_back(fallback_location: root_path)
-    else
-      flash.now[:alert] = "friendship was failed to connect"
-      redirect_back(fallback_location: root_path)
-    end
+    Friendship.where(user_id: params[:friend_id], friend_id: current_user).update(status: 'connect')
   end
 
   def destroy
     @friendship = current_user.friendships.where(friend_id: params[:id]) 
     @friendship.destroy_all 
-    flash[:alert] = "Friendship canceled"
+    flash[:alert] = "Friendship canceled!"
     redirect_back(fallback_location: root_path)
   end
+
+  private
+
+  def find_friend
+    @friend = User.find(params[:friend_id])
+  end
+
 end
