2f9efd664a0592613b89633b59df3d4ad830b466
Cindy Liu <cindyliu923@gmail.com>
2018-05-05 23:39:13 +0800

    add user name to registration new form

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,13 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
-  private
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:name])    
+  end
 
   def sort_by
     sort_by = (params[:order] == 'replies_count') ? 'replies_count DESC' :
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
   mount_uploader :avatar, ImageUploader
+  validates_presence_of :name 
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
