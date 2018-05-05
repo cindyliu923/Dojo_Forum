d3137678469834b118c7861d41496cee1a298a38
Cindy Liu <cindyliu923@gmail.com>
2018-05-06 00:35:43 +0800

    modify style and add default avatar

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-  mount_uploader :avatar, ImageUploader
+  mount_uploader :avatar, AvatarUploader
   validates_presence_of :name 
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
