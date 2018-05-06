30b15814521640fe2186fc7e92cb92373ba7350f
Cindy Liu <cindyliu923@gmail.com>
2018-05-06 23:52:16 +0800

    add index action for post api

--- app/controllers/api/v1/posts_controller.rb
+++ app/controllers/api/v1/posts_controller.rb
@@ -1,2 +1,14 @@
 class Api::V1::PostsController < ApiController
+  def index
+    if params[:category_id]
+      @category = Category.find(params[:category_id])
+      @posts = @category.posts.all_publish(current_user)
+    else
+      @posts = Post.all_publish(current_user)
+    end
+    render json: {
+      data: @posts
+    }
+  end
+
 end
